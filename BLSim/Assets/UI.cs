e458118fea22d589586ad7cfb7b8693b45ceef56
Brandon Surmanski <b.surmanski@gmail.com>
2015-06-02 12:42:04 -0400

    added logo

---
+++ BLSim/Assets/UI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+	public Texture2D brickButton; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI() {
+		Rect brickRect = new Rect(0, 0, 40, 40);
+		GUI.Button (brickRect, brickButton);
+	}
+}
