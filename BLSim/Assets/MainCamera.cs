2ab6cbbfd38c6f5572b78c541feecf0b313fcdd3
Brandon Surmanski <b.surmanski@gmail.com>
2015-06-02 14:52:20 -0400

    proper turntable camera

--- BLSim/Assets/MainCamera.cs
+++ BLSim/Assets/MainCamera.cs
@@ -9,13 +9,19 @@ public class MainCamera : MonoBehaviour {
 	public float vRotateSpeed = 1.0f;
 	public float hRotateSpeed = 1.0f;
 
+	Vector3 offset;
 	Vector3 rotation;
 
 	void Start () {
 		lastMousePosition = Input.mousePosition;
-		rotation = new Vector3 ();
+		offset = transform.position;
+		rotation = transform.rotation.eulerAngles;
 	}
-	
+
+	Vector3 offsetPosition(Quaternion rotation, Vector3 offset) {
+		return rotation * (offset.z * Vector3.forward) + offset.y * Vector3.up + offset.x * Vector3.right;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		Vector3 mouseDelta = lastMousePosition - Input.mousePosition;
@@ -23,8 +29,12 @@ public class MainCamera : MonoBehaviour {
 		mouse1Down = !Input.GetMouseButtonUp(1) && (Input.GetMouseButtonDown(1) || mouse1Down);
 
 		if (mouse1Down) {
-			rotation += new Vector3(mouseDelta.y * vRotateSpeed, mouseDelta.x * hRotateSpeed, 0);
-			transform.rotation = Quaternion.Euler(rotation);
+			rotation += new Vector3(mouseDelta.y * vRotateSpeed, -mouseDelta.x * hRotateSpeed, 0);
+
+
+			Quaternion rot = Quaternion.Euler(rotation);
+			transform.position = offsetPosition(rot, offset); 
+			transform.rotation = rot;
 		}
 
 		lastMousePosition = Input.mousePosition;
