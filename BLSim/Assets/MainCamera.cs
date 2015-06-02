9ce3a68e1bd41cac227ed65b9d7ed9a3a05c56d5
Brandon Surmanski <b.surmanski@gmail.com>
2015-06-02 13:31:59 -0400

    added camera rotation
    use RMB to rotate camera

---
+++ BLSim/Assets/MainCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	bool mouse1Down;
+	Vector3 lastMousePosition;
+
+	public float vRotateSpeed = 1.0f;
+	public float hRotateSpeed = 1.0f;
+
+	Vector3 rotation;
+
+	void Start () {
+		lastMousePosition = Input.mousePosition;
+		rotation = new Vector3 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouseDelta = lastMousePosition - Input.mousePosition;
+
+		mouse1Down = !Input.GetMouseButtonUp(1) && (Input.GetMouseButtonDown(1) || mouse1Down);
+
+		if (mouse1Down) {
+			rotation += new Vector3(mouseDelta.y * vRotateSpeed, mouseDelta.x * hRotateSpeed, 0);
+			transform.rotation = Quaternion.Euler(rotation);
+		}
+
+		lastMousePosition = Input.mousePosition;
+	}
+}
