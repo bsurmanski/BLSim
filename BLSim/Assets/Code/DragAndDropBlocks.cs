8ca1111405e976a4a45515d9e01ef064dfa1f01b
David Stoddard <stod0021@algonquinlive.com>
2015-06-09 10:26:16 -0400

    Initial UI concept and drag and drop blocks implemented.
    Very basic at the moment, now working on snapping the blocks and adding
    draganddrop from the UI to create a new block.

---
+++ BLSim/Assets/Code/DragAndDropBlocks.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragAndDropBlocks : MonoBehaviour {
+
+	private GameObject blockObjects;
+	void Start(){
+		blockObjects = GameObject.Find ("Blocks");
+		//Debug.Log (blockObjects);
+	}
+
+	private Color mouseOverColor = Color.blue;
+	private Color originalColor = Color.white;
+	private bool dragging = false;
+	private float distance;
+	private float direction;
+	/*
+	OnMouseEnter() Turns the block to a new highlighted color
+	 */
+	void OnMouseEnter()
+	{
+		GetComponent<Renderer>().material.color = mouseOverColor;
+	}
+	
+	/*
+	OnMouseExit() Returns the block to original color when mouse isn't on top of it.
+	 */
+	void OnMouseExit()
+	{
+		GetComponent<Renderer>().material.color = originalColor;
+	}
+
+	/*
+	OnMouseUp() left mouse click is not clicked
+	 */
+	void OnMouseUp()
+	{
+		dragging = false;
+	}
+
+	void OnMouseDown()
+	{
+		distance = Vector2.Distance(transform.position, Camera.main.transform.position);//Change x and y
+		dragging = true;
+	}
+	
+
+
+//	void OnMouseUpAsButton()
+//	{
+//		//Getcom
+//	}
+	
+	void Update()
+	{
+		if (dragging)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Vector3 rayPoint = ray.GetPoint(distance);
+			transform.position = rayPoint;
+		}
+
+
+	}
+
+}
